65c153bae314ef08b38e77cdcab6209aaebb0f43
imran farooq <imran_farooq7@hotmail.com>
2026-03-24 04:11:14 +0500

    root layout updated

--- components/providers.tsx
+++ components/providers.tsx
@@ -1,8 +1,8 @@
 // components/providers.tsx
 "use client";
 
-import { Toaster } from "@/components/ui/toaster";
 import { ThemeProvider } from "./theme-provider";
+import { Toaster } from "@/components/ui/sonner";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
